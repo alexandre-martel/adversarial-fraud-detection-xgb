33f5d578ba72668a2d7b52cf4194e4e48efc0b0b
MARTEL ALEXANDRE (EQUANS) <alexandre.martel75004@gmail.com>
2026-03-14 14:51:24 +0100

    Add utils func + REadme struct

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# adversarial-fraud-detection-xgb
-A credit card fraud detection project using XGBoost and LogReg that implements defenses against three issues: adversarial attacks, privacy attacks, and bias mitigation
+# Adversarial Fraud Detection XGBoost & Logistic Regression
+
+A credit card fraud detection project using XGBoost and Logistic Regression that implements defenses against three issues: adversarial attacks (FGSM), privacy attacks, and bias mitigation
+
+## Explanations and Mathematical Theory
+
+### Adversarial Attack (FGSM) /Defense Method (Adversarial training)
+
+#### Adversarial Attack : FGSM
+#### Adversarial Defense : Adversarial training
+#### Results
+
+### Privacy Attack (Membership Ineference Attack) /Defense Method (DP-SGD)
+
+#### Privacy Attack : Membership Ineference Attack
+#### Privacy Defense : DP-SGD
+#### Results
+
+
+### Bias & Mitigation 
+
+## Download and Run
+
--- src/utils.py
+++ src/utils.py
@@ -17,6 +17,47 @@ def load_dataset(csv_path):
     feat_names = [c for c in df.columns if c != 'Class']
     return X, y, feat_names
 
+# Stratification is necessary : indeed, the dataset is very unbalanced, and we want to ensure that 
+# the train, validation and test sets have the same class distribution as the original dataset.
+def get_datasets(X, y, test_size=0.2, val_size=0.2, random_state=9):
+
+    X_trainval, X_test, y_trainval, y_test = train_test_split(
+        X, y, test_size=test_size, stratify=y, random_state=random_state)
+
+    val_rel = val_size / (1.0 - test_size)
+
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_trainval, y_trainval, test_size=val_rel, stratify=y_trainval, random_state=random_state)
+    
+
+    return X_train, y_train, X_val, y_val, X_test, y_test
+
+# Standardization done on train, then applied on val and test. 
+# We also compute the 0.5% and 99.5% quantiles on the train set :
+    # Indeed, when creating FGSM attacks, we're going to clip the perturbed samples to be within these 
+    # quantiles, to avoid creating unrealistic samples, that would be too simple to change the prediction.
+def standardize(X_train, X_val, X_test):
+
+    scaler = StandardScaler()
+    X_tr_s = scaler.fit_transform(X_train)
+    X_val_s = scaler.transform(X_val)
+    X_te_s = scaler.transform(X_test)
+
+    q_low = np.quantile(X_tr_s, 0.005, axis=0)
+    q_high = np.quantile(X_tr_s, 0.995, axis=0)
+    
+    return X_tr_s, X_val_s, X_te_s, scaler, q_low, q_high
+
+# In order to give more weights to the minority class (the positive class), we can compute the 
+# scale_pos_weight, which is the ratio of the number of negative samples to the number of positive samples. 
+# Usefull for unbalanced datasets.
+def compute_scale_pos_weight(y):
+    pos = (y == 1).sum()
+    neg = (y == 0).sum()
+    if pos == 0:
+        return 1.0
+    return float(neg) / float(pos)
+
 def summarize_classification(y_true, y_pred, y_proba=None, title="Metrics"):
 
     print("\n=== " + title + " ===")
